a75536d8c7f6698ae5528f2683abbdd2a03f3e94
Joaquin <joaquinsouthwell1999@gmail.com>
2019-08-15 21:01:34 -0300

    Se creo la ruta para crear un producto y listarlos

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import{Producto} from './models/producto';
 import { MongoClient, ObjectId } from 'mongodb';
 import * as bodyparser from 'body-parser';
 const conexión = new MongoClient('mongodb://localhost:27017', { useNewUrlParser: true });
@@ -7,12 +8,58 @@ import { Resolver } from 'dns';
 const app = express();
 app.use(bodyparser.json());
 const port = 3000;
-let nombredb = "pruebadb";
+let nombredb = "almacen-backend";
 let nombrecoleccion = "Productos";
 app.get('/',async (req:Request, res:Response)=>{
     res.status(200).send('Funcionando almacen-backend');
+})
+app.post('/producto', async (req: Request, res: Response) => {
+    if (req.body.nombre && req.body.cantidad && req.body.marca&& req.body.precio && req.body.codigo_de_barras) {
+        const producto1: Producto = {
+            nombre: req.body.nombre,
+            cantidad: req.body.cantidad,
+            marca:req.body.marca,
+            precio: req.body.precio,
+            codigo_de_barras: req.body.codigo_de_barras
+        }
+        const db = conexión.db(nombredb);
+        const productos = db.collection(nombrecoleccion);
+        try {
+            await productos.insertOne(producto1);
+            console.log("Producto agregado");
+            res.send("Producto agregado a la base de datos");
+
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    } else {
+        console.log("No se encontraron todas las propiedades necesarias");
+        res.status(400).send("Falta una de las propiedades del producto");
+    }
 
 })
+
+
+
+
+
+
+app.get('/listar', async(req:Request, res:Response)=>{
+    const db = conexión.db(nombredb);
+    const productos = db.collection(nombrecoleccion);
+    console.log(req.query);
+        try {
+            const arregloproductos = await productos.find(req.query).toArray();
+            console.log(arregloproductos);
+            res.json(arregloproductos);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: err });
+        }
+        
+})
+
 
 
 
--- models/producto.ts
+++ models/producto.ts
@@ -1,6 +1,7 @@
 export interface Producto{
     nombre:string;
     cantidad:number;
+    marca:string;
     precio:number;
     codigo_de_barras:number;
 }
